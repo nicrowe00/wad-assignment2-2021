b2ecf5342ac022bd22f0ab26a109c2ab844706e1
Niall Crowe <20089334@mail.wit.ie>
2021-12-29 20:07:21 +0000

    Topic 10 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -8,15 +8,16 @@ dotenv.config();
 
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
-  console.log('load user Data');
-  try {
-    await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
-    console.info(`${users.length} users were successfully stored.`);
-  } catch (err) {
-    console.error(`failed to Load user Data: ${err}`);
+    console.log('load user Data');
+    try {
+      await userModel.deleteMany();
+      await users.forEach(user => userModel.create(user));
+      console.info(`${users.length} users were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load user Data: ${err}`);
+    }
   }
-}
+  
 async function loadGenres(){
     console.log('load movie Genres');
     try{
