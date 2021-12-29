b83a243775b608252c414f5817bc2c5e2e0df81e
Niall Crowe <20089334@mail.wit.ie>
2021-12-29 11:54:06 +0000

    node-lab1, simple server

---
+++ node-lab1/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": 1,
+        "no-console": "off"
+    }
+};
