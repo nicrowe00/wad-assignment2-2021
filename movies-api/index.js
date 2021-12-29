1d1752f28118356241903e5aae367721324ba066
Niall Crowe <20089334@mail.wit.ie>
2021-12-29 20:20:14 +0000

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,8 +5,8 @@ import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import './db';
 import './seedData';
-import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
+
 
 dotenv.config();
 
@@ -25,13 +25,10 @@ const errHandler = (err, req, res, next) => {
 
 app.use(express.json());
 
-app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true
-  }));
+app.use(passport.initialize());
 
-app.use('/api/movies', authenticate, moviesRouter);
+
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.use('/api/genres', genresRouter);
 
