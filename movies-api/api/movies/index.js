3aa654414062585a50bd0aed303a577fea7de42c
Niall Crowe <20089334@mail.wit.ie>
2021-12-30 16:53:31 +0000

    Pagination Exercise.

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,13 +2,23 @@ import express from 'express';
 import { movieReviews} from './moviesData';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
-import uniqid from 'uniqid'
+import uniqid from 'uniqid';
 
 const router = express.Router(); 
 
 router.get('/', asyncHandler(async (req, res) => {
-    const movies = await movieModel.find();
-    res.status(200).json(movies);
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const movies = await moviesPromise;
+
+    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
 }));
 
 router.get('/:id', asyncHandler(async (req, res) => {
