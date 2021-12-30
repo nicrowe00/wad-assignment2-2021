bf477b2ebee184e57342666817f6bc3dd0d24eb8
Niall Crowe <20089334@mail.wit.ie>
2021-12-30 15:26:06 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,5 +1,7 @@
 import userModel from '../api/users/userModel';
 import moviesGenres from '../api/genres/moviesGenres';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js'; 
 import users from './users';
 import genres from './genres';
 import dotenv from 'dotenv';
@@ -29,7 +31,21 @@ async function loadGenres(){
     }
 }
 
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
+
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
+  loadMovies();
 }
+
